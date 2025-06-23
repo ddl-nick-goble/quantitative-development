f6cb36a85935fd178eaf2ed5b00ca89ffe90dbc2
Nick Goble <nick.goble@dominodatalab.com>
2025-06-23 13:34:38 -0700

    Pushed from Domino: https://se-demo.domino.tech/workspace/nick_goble/quantitative-development?executionId=68598528b0c3316ebeb21d36

--- models/covariance/garch.py
+++ models/covariance/garch.py
@@ -1,45 +1,32 @@
-import numpy as np
-from sklearn.base import BaseEstimator
-from arch.univariate import arch_model
+import warnings
+from arch.univariate.base import DataScaleWarning
+from sklearn.exceptions import ConvergenceWarning
+warnings.filterwarnings("ignore", DataScaleWarning)
+warnings.filterwarnings("ignore", ConvergenceWarning)
 
 class GARCHCovarianceModel(BaseEstimator):
-    """
-    Multivariate covariance via univariate GARCH(1,1) + static correlation.
-    
-    - Fits a GARCH(1,1) to each column of X.
-    - Computes standardized residuals, then sample correlation.
-    - Builds covariance at final time: D_t · R · D_t, with D_t = diag(sigma_t).
-    """
-    name = "GARCH(1,1)"
-
-    def __init__(self, p: int = 1, q: int = 1):
-        self.p = p
-        self.q = q
+    def __init__(self, p=1, q=1, scale=100):
+        self.p, self.q, self.scale = p, q, scale
         self._cov = None
 
     def fit(self, X, y=None):
-        """
-        X : array-like, shape (n_obs, n_series)
-        """
-        X = np.asarray(X)
+        X = np.asarray(X) * self.scale             # <-- manual scaling
         n_obs, n_ser = X.shape
-
-        # store conditional volatilities & standardized residuals
         cond_vol = np.zeros((n_obs, n_ser))
-        std_resid = np.zeros((n_obs, n_ser))
+        std_resid = np.zeros_like(cond_vol)
 
         for i in range(n_ser):
-            # zero‐mean GARCH(p,q)
-            g = arch_model(X[:, i], mean="zero", vol="GARCH",
-                           p=self.p, q=self.q, dist="normal")
-            res = g.fit(disp="off")
-            cond_vol[:, i] = res.conditional_volatility
-            std_resid[:, i] = res.resid / cond_vol[:, i]
+            g = arch_model(
+                X[:, i],
+                mean="zero", vol="GARCH", p=self.p, q=self.q,
+                dist="normal", rescale=False        # <-- disable auto‐rescale
+            )
+            res = g.fit(disp="off", options={"maxiter": 200})
+            vol = res.conditional_volatility / self.scale  # <-- undo scale
+            cond_vol[:, i] = vol
+            std_resid[:, i] = res.resid / res.conditional_volatility
 
-        # static correlation of standardized residuals
         corr = np.corrcoef(std_resid.T)
-
-        # covariance at last time point
         last_sigma = cond_vol[-1, :]
         D = np.diag(last_sigma)
         self._cov = D @ corr @ D
